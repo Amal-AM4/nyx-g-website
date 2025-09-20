d1418b216da9e1de9f9fc20711f06919d44d54bd
Amal A M <vectorhero007@gmail.com>
2025-09-20 16:03:06 +0530

    768 complete and modal to

--- assets/js/menuModal.js
+++ assets/js/menuModal.js
@@ -1,13 +1,18 @@
 const menuBtn = document.getElementById('menu-btn');
 const menuModal = document.getElementById('menu-modal');
+const closeBtn = document.getElementById('close-btn');
 
 menuBtn.addEventListener('click', () => {
-    menuModal.classList.toggle('active');
+    menuModal.classList.toggle('active-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+    menuModal.classList.remove('active-modal');
 });
 
 // Close modal when link clicked
 document.querySelectorAll('.modal a').forEach(link => {
     link.addEventListener('click', () => {
-    menuModal.classList.remove('active');
+        menuModal.classList.remove('active-modal');
     });
 });
