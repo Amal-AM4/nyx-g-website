0f0da598080778a35f305ad04cd8cc715c9373e0
Amal A M <vectorhero007@gmail.com>
2025-09-21 07:45:12 +0530

    floating whatsapp completed

---
+++ assets/js/floatingWhatsApp.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#myWhatsAppButton').floatingWhatsApp({
+    phone: '918075299870',
+    popupMessage: 'Hi there. I would like to schedule a meeting to discuss your products and services.',
+    showPopup: true,
+    headerTitle: 'NYX-G Store',
+    // headerColor: 'darkgreen',
+    position: 'right',
+    });
+});
